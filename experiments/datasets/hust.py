2495bc0a30c32e48c6d94c8711eaa868544ccdb6
rochasantos <adriano_angelo@live.com>
2023-12-29 15:08:36 -0300

    completion of hust.py v1 modal

--- experiments/datasets/hust.py
+++ experiments/datasets/hust.py
@@ -2,15 +2,11 @@
 Class definition of HUST Bearing dataset download and acquisitions extraction.
 """
 
-import urllib.request
 import scipy.io
 import numpy as np
 import os
-from sklearn.model_selection import KFold, GroupKFold, StratifiedShuffleSplit, GroupShuffleSplit
-import shutil
 import zipfile
 import sys
-import ssl
 import requests
 from tqdm import tqdm
 import os
@@ -19,9 +15,8 @@ from pathlib import Path
 import requests
 import csv
 import re
+import pandas as pd
 
-# Unpack Tools
-from pyunpack import Archive
 
 # Code to avoid incomplete array results
 np.set_printoptions(threshold=sys.maxsize)
@@ -107,23 +102,7 @@ def create_metadata_file(data_dir, target_dir):
     print('Metadata file created successfully.')
     
 
-
-
-# DATA FOR TEST
-        
-# url = "https://prod-dcd-datasets-cache-zipfiles.s3.eu-west-1.amazonaws.com/cbv7jyx4p9-2.zip"
-# dirname = "hust_raw"
-# zip_name = "hust_bearing.zip"
-# relative_path = os.path.join("HUST bearing", "HUST bearing dataset")
-# zip_file_path = os.path.join(dirname, zip_name)
-
-# download_file(url, "hust_raw", "hust_bearing.zip")
-# extract_zip(zip_file_path, "hust_raw", relative_path)
-# create_metadata_file( "hust_raw/HUST bearing/HUST bearing dataset", 
-#                                  "hust_raw/hust_bearings.csv" )
-        
-
-class CWRU():
+class HUST():
     """
     CWRU class wrapper for database download and acquisition.
 
@@ -148,13 +127,21 @@ class CWRU():
     def __init__(self, bearing_names_file="hust_bearings.csv"):
         self.rawfilesdir = "hust_raw/HUST bearing/HUST bearing dataset"
         self.url = "https://prod-dcd-datasets-cache-zipfiles.s3.eu-west-1.amazonaws.com/cbv7jyx4p9-2.zip"
-        self.sample_size = 4096
-        self.bearing_names_file = bearing_names_file
-        self.bearing_labels, self.bearing_names = self.get_hust_bearings()
+        # Files Paths ordered by bearings
 
+        self.bearing_names_file = bearing_names_file
+        self.bearing_labels, self.bearing_names = self.get_hust_bearings()      
+
+        self.sample_size = pow(2, 16)
         self.signal_data = np.empty((0, self.sample_size))
         self.labels = []
         self.keys = []
+        self.df = pd.DataFrame()
+        
+        files_path = {}
+        for key, bearing in zip(self.bearing_labels, self.bearing_names):
+            files_path[key] = os.path.join(self.rawfilesdir, bearing)
+        self.files = files_path
 
         """
         Associate each file name to a bearing condition in a Python dictionary. 
@@ -164,20 +151,9 @@ class CWRU():
         of bearing at 3 working conditions. The sample rate is 51,200 samples per second.
         """
 
-        # Files Paths ordered by bearings
-        files_path = {}
-
-        for key, bearing in zip(self.bearing_labels, self.bearing_names):
-            files_path[key] = os.path.join(self.rawfilesdir, bearing)
-
-        self.files = files_path
 
     def get_hust_bearings(self):
-        # Get bearings to be considered
-
         bearing_file = os.path.join("hust_raw", self.bearing_names_file)
-        print('bearing file:', bearing_file)
-
         bearing_label = []
         bearing_file_names = []
 
@@ -188,8 +164,8 @@ class CWRU():
                 bearing_label = np.append(bearing_label, row[0])
                 bearing_file_names = np.append(bearing_file_names, row[1])
 
-        return bearing_label, bearing_file_names    
-    
+        return bearing_label, bearing_file_names
+   
 
     def download(self):
         """
@@ -209,48 +185,51 @@ class CWRU():
         extract_zip(zip_file, dirname)
         create_metadata_file(self.rawfilesdir, csv_file)
 
-    # def load_acquisitions(self):
-    #     """
-    #     Extracts the acquisitions of each file in the dictionary files_names.
-    #     """
-    #     cwd = os.getcwd()
+    def load_acquisitions(self):
+        """
+        Extracts the acquisitions of each file in the dictionary files_names.
+        """
+      
+        cwd = os.getcwd()
 
-    #     for key in self.files:
-    #         matlab_file = scipy.io.loadmat(os.path.join(cwd, self.files[key]))            
-    #         acquisition = []
-    #         for position in ['DE', 'FE', 'BA']:
-    #             keys = [key for key in matlab_file if key.endswith(position + "_time")]
-    #             if len(keys) > 0:
-    #                 array_key = keys[0]
-    #                 acquisition = matlab_file[array_key].reshape(1, -1)[0]
-    #         for i in range(len(acquisition)//self.sample_size):
-    #             sample = acquisition[(i * self.sample_size):((i + 1) * self.sample_size)]
-    #             self.signal_data = np.append(self.signal_data, np.array([sample]), axis=0)
-    #             self.labels = np.append(self.labels, key[0])
-    #             self.keys = np.append(self.keys, key)
+        # get the types of defects
+        pattern = r'\b([A-Za-z]+)\.(\d+)\.(\d+)'
+        defects = []
+        for defect in list(self.files.keys()):
+            match = re.search(pattern, defect)
+            defects.append(match.group(1))
 
+        defects = list(set(defects))
+        print("defects: ", defects)
 
-#     def get_acquisitions (self, n_samples_acquisitions=None):
-
-#         if len(self.signal_data) == 0:
-#             self.load_acquisitions()
-
-#         if not n_samples_acquisitions:
-#             return self.signal_data, self.labels
-
-#         # get the first index of each feature
-#         label_names = list(set(self.labels))        
-#         n_samples_per_label = n_samples_acquisitions // len(label_names)
-
-#         index_list = tuple()
-#         for label in label_names:
-#             index_list = index_list + (np.where(self.labels == label)[0][:n_samples_per_label],)
         
-#         indexes = np.concatenate(index_list, axis=0)
+        for key in self.files:
+            matlab_file = scipy.io.loadmat(os.path.join(cwd, self.files[key]))
+            
+            data = matlab_file["data"].reshape(1, -1)[0] [:self.sample_size]
+            defect = re.search(pattern, key).group(1)
+            
+            self.labels = np.append(self.labels, defect)
+            self.signal_data = np.vstack((self.signal_data, data))
+            self.keys = np.append(self.keys, key)       
 
-#         # print('CWRU')
-#         # print('labels ----', self.labels[indexes].shape)
-#         # print('signal ----', self.signal_data[indexes].shape)
 
-#         return self.signal_data[indexes], self.labels[indexes]
+    def get_acquisitions (self, n_samples_acquisitions=None):
 
+        if len(self.signal_data) == 0:
+            self.load_acquisitions()
+
+        if not n_samples_acquisitions:
+            return self.signal_data, self.labels
+
+        # get the first index of each feature
+        label_names = list(set(self.labels))        
+        n_samples_per_label = n_samples_acquisitions // len(label_names)
+
+        index_list = tuple()
+        for label in label_names:
+            index_list = index_list + (np.where(self.labels == label)[0][:n_samples_per_label],)
+        
+        indexes = np.concatenate(index_list, axis=0)
+
+        return self.signal_data[indexes], self.labels[indexes]
